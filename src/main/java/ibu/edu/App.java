fc9e50a4809dc2d263ccf9cd2f7f136b53e792ce
MakAlijevic <alijevic.mak@gmail.com>
2022-04-10 19:13:30 +0200

    Created IPAddress class

--- src/main/java/ibu/edu/App.java
+++ src/main/java/ibu/edu/App.java
@@ -2,8 +2,7 @@ package ibu.edu;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ){
+
     }
 }
